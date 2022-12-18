8f961499a0c26fb952f0baf249858ae975852e50
Vishal Bhandari <vishaljbhandari@gmail.com>
2022-12-19 02:51:45 +0530

    Initial Code Commit

---
+++ Application/Basic/Comments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+// Single Line Comment
+
+/*
+ * 
+ * Multiline Comment
+ * 
+ */
+
+namespace Application.Basic
+{
+    class Comments
+    {
+        // int a; // commenting a code line
+    }
+}
