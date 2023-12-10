15d2d8af385c0e60f6d5fc1ba1c9e13370fb5d3b
Vishal Bhandari <vishaljbhandari@gmail.com>
2023-12-11 02:27:31 +0530

    updating

---
+++ Application/UnsafeCodes/UnsafeCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UnsafeCodes
+{
+    class UnsafeCode
+    {
+        /*
+         * C# allows using pointer variables in a function of code block when it is marked by the unsafe modifier.
+         */
+
+        public static void Example()
+        {
+
+        }
+    }
+}
