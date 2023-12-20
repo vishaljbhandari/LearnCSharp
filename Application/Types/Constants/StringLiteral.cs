2d822fe01bcec467c4a5ddb0e8b47bd6caf76100
Vishal Bhandari <vishaljbhandari@gmail.com>
2023-12-20 15:13:45 +0530

    Updating

--- Application/Types/Constants/StringLiteral.cs
+++ Application/Types/Constants/StringLiteral.cs
@@ -8,9 +8,37 @@ namespace Application.Types.Constants
 {
     class StringLiteral
     {
+        /*
+         * String literals or constants are enclosed in double quotes "" or with @""
+         * 
+         * A string contains characters that are similar to character literals: plain characters, escape sequences, and universal characters.
+         * 
+         * You can break a long line into multiple lines using string literals and separating the parts using whitespaces.
+         */
+        /*
+         *  Examples
+         
+            "hello, dear"
+            "hello, \
+            dear"
+            "hello, " "d" "ear"
+            @"hello dear"
+
+         */
         public static void Example()
         {
+            const string one = "Amit";
+            // displaying first constant string
+            Console.WriteLine(one);
 
+            const string two = "Tom";
+            const string three = "Steve";
+
+            // compile-time error
+            // one = "David";
+
+            Console.WriteLine(two);
+            Console.WriteLine(three);
         }
     }
 }
